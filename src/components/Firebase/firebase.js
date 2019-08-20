586f18ddada5e8923129460b481de1296683a488
Benjamin West <32368718+benwestdev@users.noreply.github.com>
2019-08-20 14:25:36 -0400

    add firebase database

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from "firebase/app";
 import "firebase/auth";
+import "firebase/database";
 
 import { DEV_CONFIG, PROD_CONFIG } from "./config";
 
@@ -10,6 +11,7 @@ class Firebase {
     app.initializeApp(config);
 
     this.auth = app.auth();
+    this.db = app.database();
   }
 
   // *** Auth API ***
@@ -24,6 +26,10 @@ class Firebase {
   doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
 
   doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+
+  // *** User API ***
+  user = uid => this.db.ref(`users/${uid}`);
+  users = () => this.db.ref("users");
 }
 
 export default Firebase;
