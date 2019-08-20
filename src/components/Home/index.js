b50cd523934cd03d17a58c31352ebb9d807a4602
Benjamin West <32368718+benwestdev@users.noreply.github.com>
2019-08-20 13:27:59 -0400

    add temp home page

---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Header } from "semantic-ui-react";
+
+const HomePage = () => (
+  <Container className="body-container">
+    <Header as="h1" textAlign="center">
+      Home Page
+    </Header>
+  </Container>
+);
+
+export default HomePage;
