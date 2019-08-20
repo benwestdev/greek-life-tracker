8d597ca6d40e5b2ae944cd527859a4f3558f9dcb
Benjamin West <32368718+benwestdev@users.noreply.github.com>
2019-08-20 12:29:47 -0400

    add firebase auth api

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,28 +1,29 @@
 import app from "firebase/app";
+import "firebase/auth";
 
-const devConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
-};
-const prodConfig = {
-  apiKey: process.env.REACT_APP_PROD_API_KEY,
-  authDomain: process.env.REACT_PROD_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_PROD_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROD_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_PROD_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_PROD_MESSAGING_SENDER_ID
-};
+import { DEV_CONFIG, PROD_CONFIG } from "./config";
 
-const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
+const config = process.env.NODE_ENV === "production" ? PROD_CONFIG : DEV_CONFIG;
 
 class Firebase {
   constructor() {
     app.initializeApp(config);
+
+    this.auth = app.auth();
   }
+
+  // *** Auth API ***
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
