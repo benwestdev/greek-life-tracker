82569aa99be25d7f7aae20eefae1bc59d18ccedd
Benjamin West <32368718+benwestdev@users.noreply.github.com>
2019-08-20 12:28:27 -0400

    comment out unused routes for the moment, add some styling

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,27 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import "./style.css";
 import * as ROUTES from "../../constants/routes";
 
 import Navigation from "../Navigation";
-import LandingPage from "../Landing";
+// import LandingPage from "../Landing";
 import SignUpPage from "../SignUp";
-import SignInPage from "../SignIn";
-import PasswordForgetPage from "../PasswordForget";
-import AccountPage from "../Account";
-import AdminPage from "../Admin";
+// import SignInPage from "../SignIn";
+// import PasswordForgetPage from "../PasswordForget";
+// import AccountPage from "../Account";
+// import AdminPage from "../Admin";
 
 const App = () => (
   <Router>
     <div>
       <Navigation />
     </div>
-    <Route exact path={ROUTES.LANDING} component={LandingPage} />
+    {/* <Route exact path={ROUTES.LANDING} component={LandingPage} /> */}
     <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-    <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+    {/* <Route path={ROUTES.SIGN_IN} component={SignInPage} />
     <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
     <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-    <Route path={ROUTES.ADMIN} component={AdminPage} />
+    <Route path={ROUTES.ADMIN} component={AdminPage} /> */}
   </Router>
 );
 export default App;
