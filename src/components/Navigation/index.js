5fc3d908f33f38b34940c1b6f845292882801c77
Benjamin West <32368718+benwestdev@users.noreply.github.com>
2019-08-20 13:28:13 -0400

    add auth vs nonauth menu bar

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,7 +3,27 @@ import { Link } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
 import * as ROUTES from "../../constants/routes";
 
-const Navigation = () => (
+import SignOutButton from "../SignOut";
+
+const Navigation = ({ authUser }) => {
+  return authUser ? <NavigationAuth /> : <NavigationNonAuth />;
+};
+
+const NavigationAuth = () => (
+  <Menu inverted fixed="top">
+    <Link to={ROUTES.ACCOUNT} name="account">
+      <Menu.Item name="my account" />
+    </Link>
+    <Link to={ROUTES.ADMIN} name="admin">
+      <Menu.Item name="admin" />
+    </Link>
+    <Menu.Item position="right">
+      <SignOutButton />
+    </Menu.Item>
+  </Menu>
+);
+
+const NavigationNonAuth = () => (
   <Menu inverted fixed="top">
     <Link to={ROUTES.SIGN_IN} name="signin">
       <Menu.Item name="sign in" />
@@ -11,16 +31,6 @@ const Navigation = () => (
     <Link to={ROUTES.SIGN_UP} name="signup">
       <Menu.Item name="sign up" />
     </Link>
-    <Link to={ROUTES.LANDING} name="landing">
-      <Menu.Item name="sign in" />
-    </Link>
-    <Link to={ROUTES.ACCOUNT} name="account">
-      <Menu.Item name="my account" />
-    </Link>
-    <Link to={ROUTES.ADMIN} name="admin">
-      <Menu.Item name="admin" />
-    </Link>
   </Menu>
 );
-
 export default Navigation;
