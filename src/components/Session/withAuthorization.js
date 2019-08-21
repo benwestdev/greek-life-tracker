4a88c8d57248b53d60c335c6095eb8bc5981fd6b
Benjamin West <32368718+benwestdev@users.noreply.github.com>
2019-08-21 06:16:08 -0400

    remove uneccessary dependency

--- src/components/Session/withAuthorization.js
+++ src/components/Session/withAuthorization.js
@@ -5,7 +5,6 @@ import { compose } from "recompose";
 import AuthUserContext from "./context";
 import { withFirebase } from "../Firebase";
 import * as ROUTES from "../../constants/routes";
-import { thisExpression } from "@babel/types";
 
 const withAuthorization = condition => Component => {
   class WithAuthorization extends React.Component {
