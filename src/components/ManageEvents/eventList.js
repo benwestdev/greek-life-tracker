853d599a4e2ddce5e69df34a5579913a743b1713
Benjamin West <32368718+benwestdev@users.noreply.github.com>
2019-08-21 08:23:34 -0400

    finish up eventList component

--- src/components/ManageEvents/eventList.js
+++ src/components/ManageEvents/eventList.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { Container, Header, Card, List, Button } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { Container, Header, Card, List, Button, Grid } from "semantic-ui-react";
 
-const EventList = ({ events }) => (
-  <Card.Group>
-    {events.map(event => (
-      <EventItem key={event.uid} event={event} />
-    ))}
-  </Card.Group>
+import * as ROUTES from "../../constants/routes";
+
+const EventList = ({ events, onDelete }) => (
+  <Container style={{ marginTop: "40px" }}>
+    <Card.Group centered>
+      {events.map(event => (
+        <EventItem key={event.uid} event={event} onDelete={onDelete} />
+      ))}
+    </Card.Group>
+  </Container>
 );
 
-const EventItem = ({ event }) => (
+const EventItem = ({ event, onDelete }) => (
   <Card>
     <Card.Content>
       <Card.Header>{event.name}</Card.Header>
@@ -17,53 +22,58 @@ const EventItem = ({ event }) => (
       <Card.Description>
         <EventDetailsView event={event} />
       </Card.Description>
-      <div className="ui two buttons">
-        {/* <Link to={`/admin/events/${event.id}`}>
-              <Button basic size="tiny" color="green">
-                View/Edit
-              </Button>
-            </Link> */}
-        <Button
-          basic
-          size="tiny"
-          color="red"
-          //   onClick={() => this.handleDelete(event.id)}
-        >
-          Delete
-        </Button>
-      </div>
+
+      <Grid columns={2} style={{ marginTop: "5px" }}>
+        <Grid.Column>
+          <Button
+            className="button-theme shorter red"
+            onClick={() => onDelete(event.uid)}
+          >
+            Delete
+          </Button>
+        </Grid.Column>
+        <Grid.Column>
+          <Link to={`${ROUTES.VIEW_EVENT}/${event.uid}`}>
+            <Button className="button-theme shorter">View/Edit</Button>
+          </Link>
+        </Grid.Column>
+      </Grid>
     </Card.Content>
   </Card>
 );
 
-const EventDetailsView = ({ event }) => (
-  <Container>
-    <Header as="h3">Event Info</Header>
-    <List>
-      <List.Item>
-        <List.Icon name="group" />
-        <List.Content>{event.type}</List.Content>
-      </List.Item>
-      <List.Item>
-        <List.Icon name="calendar alternate outline" />
-        <List.Content>{event.date}</List.Content>
-      </List.Item>
-      <List.Item>
-        <List.Icon name="time" />
-        <List.Content>
-          {event.startTime} - {event.endTime}
-        </List.Content>
-      </List.Item>
-      <List.Item>
-        <List.Icon name="pin" />
-        <List.Content>{event.address}</List.Content>
-      </List.Item>
-      <List.Item>
-        <List.Icon name="percent" />
-        <List.Content>{event.points} Points</List.Content>
-      </List.Item>
-    </List>
-  </Container>
-);
+const EventDetailsView = ({ event }) => {
+  const startTime = event.startTime || "???";
+  const endTime = event.endTime || "???";
+  return (
+    <Container>
+      <Header as="h3">Event Info</Header>
+      <List>
+        <List.Item>
+          <List.Icon name="group" />
+          <List.Content>{event.type}</List.Content>
+        </List.Item>
+        <List.Item>
+          <List.Icon name="calendar alternate outline" />
+          <List.Content>{event.date}</List.Content>
+        </List.Item>
+        <List.Item>
+          <List.Icon name="time" />
+          <List.Content>
+            {startTime} - {endTime}
+          </List.Content>
+        </List.Item>
+        <List.Item>
+          <List.Icon name="pin" />
+          <List.Content>{event.address || "No address set"}</List.Content>
+        </List.Item>
+        <List.Item>
+          <List.Icon name="percent" />
+          <List.Content>{event.points} Points</List.Content>
+        </List.Item>
+      </List>
+    </Container>
+  );
+};
 
 export default EventList;
