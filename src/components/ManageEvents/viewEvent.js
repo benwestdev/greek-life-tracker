14a67e52c6ea572a3b4df01bfd7ce44889fb134d
Benjamin West <32368718+benwestdev@users.noreply.github.com>
2019-08-21 12:12:13 -0400

    clean up dependencies and add component for viewEvent

--- src/components/ManageEvents/viewEvent.js
+++ src/components/ManageEvents/viewEvent.js
@@ -1,21 +1,63 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import {
-  Container,
-  Form,
-  Dropdown,
-  Button,
-  TextArea,
-  Grid,
-  Header
-} from "semantic-ui-react";
-import { DateInput, TimeInput } from "semantic-ui-calendar-react";
+import { Container, Divider, Header, Dimmer, Loader } from "semantic-ui-react";
 
-import { AuthUserContext, withAuthorization } from "../Session";
-import * as ROUTES from "../../constants/routes";
+import { withAuthorization } from "../Session";
 import * as ROLES from "../../constants/roles";
 import { compose } from "recompose";
 import { withFirebase } from "../Firebase";
+import { EventDetailsView } from "./eventList";
+import { ApprovalList } from "./approvalList";
 
-const EventView = () => <h1>Viewing an event</h1>;
-export default EventView;
+class EventViewPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      event: {}
+    };
+  }
+
+  componentDidMount() {
+    console.log("mounting");
+    this.setState({ loading: true });
+    const eventUID = this.props.match.params.id;
+    console.log(eventUID);
+
+    this.props.firebase.event(eventUID).on("value", snapshot => {
+      this.setState({
+        event: snapshot.val(),
+        loading: false
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.props.firebase.user(this.props.match.params.id).off();
+  }
+
+  render() {
+    const { event, loading } = this.state;
+    return (
+      <Container className="body-container">
+        <Header as="h1" textAlign="center">
+          {event.name}
+        </Header>
+        {loading && (
+          <Dimmer>
+            <Loader inverted />
+          </Dimmer>
+        )}
+        <EventDetailsView event={event} />
+        <Divider />
+        <ApprovalList eventId={event.uid} />
+      </Container>
+    );
+  }
+}
+
+const condition = authUser => authUser && !!authUser.roles[ROLES.ADMIN];
+export default compose(
+  withAuthorization(condition),
+  withFirebase
+)(EventViewPage);
