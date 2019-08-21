126d416cedda545e99797fd87796075b3b0f4c6f
Benjamin West <32368718+benwestdev@users.noreply.github.com>
2019-08-21 17:23:45 -0400

    pull in attendee data

--- src/components/Events/index.js
+++ src/components/Events/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import { Container, Header, Dimmer, Loader, Button } from "semantic-ui-react";
+import moment from "moment";
 
 import { withFirebase } from "../Firebase";
 import { withAuthorization, AuthUserContext } from "../Session";
 import * as ROLES from "../../constants/roles";
+import * as STATUSES from "../../constants/statuses";
 import EventList from "./eventList";
 import { compose } from "recompose";
 
@@ -28,18 +30,37 @@ class EventsPage extends Component {
           ...eventObject[key],
           uid: key
         }));
+        eventList.forEach(event => {
+          if (event.attendances) {
+            const attendeesList = Object.keys(event.attendances).map(key => ({
+              ...event.attendances[key],
+              uid: key
+            }));
+            event.attendees = attendeesList;
+          }
+        });
         this.setState({
           allEvents: eventList,
           loading: false
         });
+        console.log(eventList);
       } else {
         this.setState({ allEvents: [], loading: false });
       }
     });
+    this.props.firebase.attendances().on("value", snapshot => {});
   }
 
   handleAttend = (event, userId) => {
     console.log("attending ", event.uid, " - ", userId);
+    console.log(STATUSES.PENDING);
+    this.props.firebase.attendances(event.uid).push({
+      [userId]: STATUSES.PENDING
+    });
+
+    this.props.firebase.userAttendances(userId).push({
+      [event.uid]: STATUSES.PENDING
+    });
   };
 
   render() {
