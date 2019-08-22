be01e93c5adec63dff72453efe8c704e5e5724ea
Benjamin West <32368718+benwestdev@users.noreply.github.com>
2019-08-22 13:37:15 -0400

    add appoval  list

--- src/components/ManageEvents/approvalList.js
+++ src/components/ManageEvents/approvalList.js
@@ -1,27 +1,64 @@
 import React, { Component } from "react";
-import { Container, Header, Dimmer, Loader } from "semantic-ui-react";
+import { Container, Header, Card, Button } from "semantic-ui-react";
 
-export class ApprovalList extends Component {
-  constructor(props) {
-    super(props);
+import * as STATUSES from "../../constants/statuses";
 
-    this.state = {
-      loading: false,
-      approvals: []
-    };
-  }
+const ApprovalListSection = ({ event, handleApproveReject }) => (
+  <Container className="body-container">
+    <Header as="h1" textAlign="center">
+      Approval List for 123 {event.name}
+    </Header>
+    <ApprovalList event={event} />
+  </Container>
+);
 
-  render() {
-    // const { approvals, loading } = this.state;
-    return (
-      <Container className="body-container">
-        <Header as="h1" textAlign="center">
-          Approval List for ({this.props.eventId})
-        </Header>
-        {}
-      </Container>
-    );
-  }
-}
-const ApprovalListPage = () => <h1>Approval Page</h1>;
-export default ApprovalListPage;
+const ApprovalList = ({ event, handleApproveReject }) => (
+  <Card.Group centered={true}>
+    {event.fullAttendances &&
+      event.fullAttendances.map((attendance, index) => {
+        return (
+          <Card key={index}>
+            <Card.Content>
+              {/* <Image floated='right' size='mini' src='/images/avatar/large/steve.jpg' /> */}
+              <Card.Header>{attendance.username}</Card.Header>
+              {/* <Card.Meta>{attendance.status}</Card.Meta> */}
+              <Card.Description>
+                Give <strong>{attendance.username}</strong>
+                <strong>{" " + event.points + " "}</strong>points for attending
+                on
+                {" " + event.date}
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <div className="ui two buttons">
+                <Button
+                  basic
+                  color="green"
+                  onClick={() =>
+                    handleApproveReject(attendance.id, STATUSES.APPROVED)
+                  }
+                >
+                  Approve
+                </Button>
+                s
+                <Button
+                  basic
+                  color="red"
+                  // onClick={() =>
+                  //   props.handleApproveReject(
+                  //     attendance.id,
+                  //     "Rejected"
+                  //   )
+                  // }
+                >
+                  Decline
+                </Button>
+              </div>
+            </Card.Content>
+          </Card>
+        );
+      })}
+  </Card.Group>
+);
+
+export default ApprovalListSection;
