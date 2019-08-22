34cba51adbda6a861972e10a97b9cbc73e29284f
Benjamin West <32368718+benwestdev@users.noreply.github.com>
2019-08-22 13:28:14 -0400

    add query to pull event and attendances

--- src/components/ManageEvents/viewEvent.js
+++ src/components/ManageEvents/viewEvent.js
@@ -3,10 +3,11 @@ import { Container, Divider, Header, Dimmer, Loader } from "semantic-ui-react";
 
 import { withAuthorization } from "../Session";
 import * as ROLES from "../../constants/roles";
+import * as STATUSES from "../../constants/statuses";
 import { compose } from "recompose";
 import { withFirebase } from "../Firebase";
 import { EventDetailsView } from "./eventList";
-import { ApprovalList } from "./approvalList";
+import ApprovalListSection from "./approvalList";
 
 class EventViewPage extends Component {
   constructor(props) {
@@ -25,10 +26,42 @@ class EventViewPage extends Component {
     console.log(eventUID);
 
     this.props.firebase.event(eventUID).on("value", snapshot => {
-      this.setState({
-        event: snapshot.val(),
-        loading: false
-      });
+      const event = snapshot.val();
+      event.uid = eventUID;
+      //TODO: Sort this by status/date in DB call
+      if (event.attendances) {
+        const attendeesList = Object.keys(event.attendances).map(key => ({
+          ...event.attendances[key]
+        }));
+        event.attendees = attendeesList;
+        const userIds = event.attendees.map(attendee => {
+          const keyList = Object.keys(attendee);
+          return keyList.length === 1 &&
+            attendee[keyList[0]] === STATUSES.PENDING
+            ? keyList[0]
+            : null;
+        });
+        console.log({ userIds });
+        const fullAttendances = [];
+        if (userIds.length > 0) {
+          const promises = userIds.map(userId =>
+            this.props.firebase.user(userId).once("value")
+          );
+          Promise.all(promises).then(results => {
+            results.forEach(result => {
+              console.log("here");
+              fullAttendances.push(result.val());
+            });
+            console.log("full: ", fullAttendances);
+            event.fullAttendances = fullAttendances;
+            console.log({ event });
+            this.setState({
+              event: event,
+              loading: false
+            });
+          });
+        }
+      }
     });
   }
 
@@ -41,7 +74,7 @@ class EventViewPage extends Component {
     return (
       <Container className="body-container">
         <Header as="h1" textAlign="center">
-          {event.name}
+          {event.name} 123
         </Header>
         {loading && (
           <Dimmer>
@@ -50,7 +83,7 @@ class EventViewPage extends Component {
         )}
         <EventDetailsView event={event} />
         <Divider />
-        <ApprovalList eventId={event.uid} />
+        {event.fullAttendances && <ApprovalListSection event={event} />}
       </Container>
     );
   }
