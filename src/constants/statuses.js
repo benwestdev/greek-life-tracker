0fbcb8a4d3c4cfc0fad369a6832ea6b3568401ff
Benjamin West <32368718+benwestdev@users.noreply.github.com>
2019-08-23 00:25:52 -0400

    add event types to constants

--- src/constants/statuses.js
+++ src/constants/statuses.js
@@ -1,3 +1,12 @@
 export const PENDING = "Pending Approval";
 export const APPROVED = "Approved";
 export const DENIED = "Denied";
+
+export const ALPHA_TYPE = "Alpha Gam";
+export const COM_SERVICE_TYPE = "Philanthropy/Community Service";
+export const SISTERHOOD_TYPE = "Sisterhood";
+
+export const EVENT_TYPES_MAP = {};
+EVENT_TYPES_MAP[ALPHA_TYPE] = 0;
+EVENT_TYPES_MAP[COM_SERVICE_TYPE] = 0;
+EVENT_TYPES_MAP[SISTERHOOD_TYPE] = 0;
