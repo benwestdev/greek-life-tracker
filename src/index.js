36b25820159d5c80652a4c8ed14ba33d61aaef14
Benjamin West <32368718+benwestdev@users.noreply.github.com>
2019-09-13 13:21:36 -0400

    remove semantic ui react stying

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 
-import "semantic-ui-css/semantic.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./theme/assets/vendor/nucleo/css/nucleo.css";
+import "./theme/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css";
+import "./theme/assets/scss/argon-dashboard-react.scss";
+
 import "./index.css";
 
 import Firebase, { FirebaseContext } from "./components/Firebase/";
